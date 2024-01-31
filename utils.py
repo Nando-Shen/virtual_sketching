28c9c305eb0e02462bb642103736dce91e24a3a7
shenjiamingnus <e1221616@u.nus.edu>
2024-01-31 19:21:08 +0800

    a

--- utils.py
+++ utils.py
@@ -99,17 +99,17 @@ def draw(f, width=128):
     z0 = (int)(1 + z0 * width // 2)
     z2 = (int)(1 + z2 * width // 2)
     canvas = np.zeros([width * 2, width * 2]).astype('float32')
-    # tmp = 1. / 100
-    # for i in range(100):
-    #     t = i * tmp
-    #     x = (int)((1-t) * (1-t) * x0 + 2 * t * (1-t) * x1 + t * t * x2)
-    #     y = (int)((1-t) * (1-t) * y0 + 2 * t * (1-t) * y1 + t * t * y2)
-    #     z = (int)((1-t) * z0 + t * z2)
-    #     w = (1-t) * w0 + t * w2
-    #     if (i == 1 or i == 99):
-    #         print('-------------x{} y{} ---------'.format(x,y))
-    #     cv2.circle(canvas, (y, x), z, w, -1)
-    cv2.line(canvas, (x0,y0), (x2,y2), (255,255,255), z0)
+    tmp = 1. / 100
+    for i in range(100):
+        t = i * tmp
+        x = (int)((1-t) * (1-t) * x0 + 2 * t * (1-t) * x1 + t * t * x2)
+        y = (int)((1-t) * (1-t) * y0 + 2 * t * (1-t) * y1 + t * t * y2)
+        z = (int)((1-t) * z0 + t * z2)
+        w = (1-t) * w0 + t * w2
+        # if (i == 1 or i == 99):
+        #     print('-------------x{} y{} ---------'.format(x,y))
+        cv2.circle(canvas, (y, x), z, w, -1)
+    # cv2.line(canvas, (x0,y0), (x2,y2), (255,255,255), z0)
 
     return 1 - cv2.resize(canvas, dsize=(width, width))
 
