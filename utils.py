27b1bf0d5abcee42a61e46ec1bdfc25bea706681
shenjiamingnus <e1221616@u.nus.edu>
2024-02-01 09:29:36 +0800

    a

--- utils.py
+++ utils.py
@@ -90,7 +90,6 @@ def draw(f, width=128):
     x0, y0, x1, y1, x2, y2, z0, z2, w0, w2 = f
     x1 = x0 + (x2 - x0) * x1
     y1 = y0 + (y2 - y0) * y1
-    cv2.line(canvas, (x0,y0), (x2,y2), (255,255,255), z0)
     x0 = normal(x0, width * 2)
     x1 = normal(x1, width * 2)
     x2 = normal(x2, width * 2)
@@ -100,17 +99,22 @@ def draw(f, width=128):
     z0 = (int)(1 + z0 * width // 2)
     z2 = (int)(1 + z2 * width // 2)
     canvas = np.zeros([width * 2, width * 2]).astype('float32')
-    # tmp = 1. / 100
-    # for i in range(100):
-    #     t = i * tmp
-    #     x = (int)((1-t) * (1-t) * x0 + 2 * t * (1-t) * x1 + t * t * x2)
-    #     y = (int)((1-t) * (1-t) * y0 + 2 * t * (1-t) * y1 + t * t * y2)
-    #     z = (int)((1-t) * z0 + t * z2)
-    #     w = (1-t) * w0 + t * w2
-    #     # if (i == 1 or i == 99):
-    #     #     print('-------------x{} y{} ---------'.format(x,y))
-    #     cv2.circle(canvas, (y, x), z, w, -1)
+    tmp = 1. / 100
+    curlist = []
+    for i in range(0, 100, 25):
+        t = i * tmp
+        x = (int)((1-t) * (1-t) * x0 + 2 * t * (1-t) * x1 + t * t * x2)
+        y = (int)((1-t) * (1-t) * y0 + 2 * t * (1-t) * y1 + t * t * y2)
+        z = (int)((1-t) * z0 + t * z2)
+        w = (1-t) * w0 + t * w2
+        # if (i == 1 or i == 99):
+        #     print('-------------x{} y{} ---------'.format(x,y))
+        # cv2.circle(canvas, (y, x), z, w, -1)
+        curlist.append((y,x))
     # cv2.line(canvas, (x0,y0), (x2,y2), (255,255,255), z0)
+    for i in range(len(curlist) - 1):
+        # pair = curlist[i], curlist[i + 1]
+        cv2.line(canvas, curlist[i], curlist[i + 1], (255, 255, 255), z0)
 
     return 1 - cv2.resize(canvas, dsize=(width, width))
 
